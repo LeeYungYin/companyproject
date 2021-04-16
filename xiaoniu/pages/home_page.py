50ae15c4cd2efa18718e79f4390a3a92a3d1aeaa
LeeYungYin <enjoyson_0909@126.com>
2021-04-16 14:40:07 +0800

    公司项目4.16

--- xiaoniu/pages/home_page.py
+++ xiaoniu/pages/home_page.py
@@ -8,19 +8,6 @@ from pages.workers_page import WorkersPage
 
 class HomePage(BasePage):
     url = config.HOST + '#/dashboard'
-    # user_account_locator = (By.XPATH, "//div[@nzplacement  = 'bottomRight']")
-    # operation_locator = (By.XPATH, "//li[text() = '运营管理']")
-    # niu_product_locator = (By.XPATH, "//span[@title = '产品管理']")
-    # niu_workers_locator = (By.XPATH, "//span[@title = '设备管理']")
-    # niu_workers_flightsheets_locator = (By.XPATH, "//span[@title = '设备飞行表汇总']")
-    # niu_profits_locator = (By.XPATH, "//span[@title = '收益管理']")
-    # niu_powers_locator = (By.XPATH, "//span[@title = '耗电管理']")
-    # niu_wallets_locator = (By.XPATH, "//span[@title = '钱包管理']")
-    # niu_localwallets_locator = (By.XPATH, "//span[@title = '本地钱包管理']")
-    # niu_wallets_address_locator = (By.XPATH, "//span[@title = '//span[@title = '钱包地址管理']']")
-    # niu_recieveaccounts_locator = (By.XPATH, "//span[@title = '//span[@title = '收款帐号']")
-    # niu_flightSheet_locator = (By.XPATH, "//span[@title = '//span[@title = '飞行表管理']")
-    # niu_farms_locator = (By.XPATH, "//span[@title = '矿场管理']")
     locator = home_locator
 
     def get(self):
